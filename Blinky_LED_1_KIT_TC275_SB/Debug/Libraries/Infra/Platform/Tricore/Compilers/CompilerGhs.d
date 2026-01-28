c2fc400d09f4ff7562859fc2a667f440cc60c9a3
SOO16 <sywl6704@gmail.com>
2026-01-28 12:08:27 +0900

    IVS_TC275_2026_01_28_V1

---
+++ Blinky_LED_1_KIT_TC275_SB/Debug/Libraries/Infra/Platform/Tricore/Compilers/CompilerGhs.d
@@ -0,0 +1,18 @@
+CompilerGhs.o :	../Libraries/Infra/Platform/Tricore/Compilers/CompilerGhs.c
+../Libraries/Infra/Platform/Tricore/Compilers/CompilerGhs.c :
+CompilerGhs.o :	"C:\Users\User\IVS_TC275\Blinky_LED_1_KIT_TC275_SB\Libraries\iLLD\TC27D\Tricore\Cpu\Std\Ifx_Types.h"
+"C:\Users\User\IVS_TC275\Blinky_LED_1_KIT_TC275_SB\Libraries\iLLD\TC27D\Tricore\Cpu\Std\Ifx_Types.h" :
+CompilerGhs.o :	"C:\Users\User\IVS_TC275\Blinky_LED_1_KIT_TC275_SB\Libraries\Infra\Platform\Tricore\Compilers\Compilers.h"
+"C:\Users\User\IVS_TC275\Blinky_LED_1_KIT_TC275_SB\Libraries\Infra\Platform\Tricore\Compilers\Compilers.h" :
+CompilerGhs.o :	"C:\Users\User\IVS_TC275\Blinky_LED_1_KIT_TC275_SB\Configurations\Ifx_Cfg.h"
+"C:\Users\User\IVS_TC275\Blinky_LED_1_KIT_TC275_SB\Configurations\Ifx_Cfg.h" :
+CompilerGhs.o :	"C:\Users\User\IVS_TC275\Blinky_LED_1_KIT_TC275_SB\Libraries\Infra\Platform\Tricore\Compilers\CompilerTasking.h"
+"C:\Users\User\IVS_TC275\Blinky_LED_1_KIT_TC275_SB\Libraries\Infra\Platform\Tricore\Compilers\CompilerTasking.h" :
+CompilerGhs.o :	"C:\Infineon\AURIX-Studio-1.10.28\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h"
+"C:\Infineon\AURIX-Studio-1.10.28\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h" :
+CompilerGhs.o :	"C:\Users\User\IVS_TC275\Blinky_LED_1_KIT_TC275_SB\Libraries\iLLD\TC27D\Tricore\Cpu\Std\Platform_Types.h"
+"C:\Users\User\IVS_TC275\Blinky_LED_1_KIT_TC275_SB\Libraries\iLLD\TC27D\Tricore\Cpu\Std\Platform_Types.h" :
+CompilerGhs.o :	"C:\Users\User\IVS_TC275\Blinky_LED_1_KIT_TC275_SB\Libraries\iLLD\TC27D\Tricore\Cpu\Std\Ifx_TypesTasking.h"
+"C:\Users\User\IVS_TC275\Blinky_LED_1_KIT_TC275_SB\Libraries\iLLD\TC27D\Tricore\Cpu\Std\Ifx_TypesTasking.h" :
+CompilerGhs.o :	..\Libraries\Infra\Platform\Tricore\Compilers\Compilers.h
+..\Libraries\Infra\Platform\Tricore\Compilers\Compilers.h :
